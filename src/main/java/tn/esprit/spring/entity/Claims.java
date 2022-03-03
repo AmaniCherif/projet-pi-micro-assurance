533b80f5dc26a2ceeb069c26a0ef0b135f3b852e
YosserBenameur <Yosser.Benameur@esprit.tn>
2022-03-03 04:01:50 +0100

    commit
    commit

--- src/main/java/tn/esprit/spring/entity/Claims.java
+++ src/main/java/tn/esprit/spring/entity/Claims.java
@@ -8,8 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+
+
 import javax.persistence.ManyToOne;
+
+
+
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -74,7 +78,7 @@ public class Claims implements Serializable {
 	}
 
 	
-	@ManyToMany
+	@ManyToOne
 	private User user;
 	
 
