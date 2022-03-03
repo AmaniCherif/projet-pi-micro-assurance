c10fba9c157faca53d8e9389a7bbb08071b2f5ea
Ameni Latrach <Ameni.latrach@esprit.tn>
2022-03-03 16:47:10 +0100

    commit
    commit

--- src/main/java/tn/esprit/spring/entity/Contract.java
+++ src/main/java/tn/esprit/spring/entity/Contract.java
@@ -19,8 +19,7 @@ public class Contract implements Serializable {
 
 	private static final long serialVersionUID= 1L;
 	@Id
-	@GeneratedValue( strategy = GenerationType.IDENTITY)
-	@Column (name= "ref_contrat")
+	@GeneratedValue
 	private String ref_contrat;
 	@Temporal (TemporalType.DATE)
 	private Date dateDebut;
