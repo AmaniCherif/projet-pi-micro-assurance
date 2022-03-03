27406f218a9febfbb17c87e2d452664fc59e4597
YosserBenameur <Yosser.Benameur@esprit.tn>
2022-03-03 21:04:25 +0100

    commit

--- src/main/java/tn/esprit/spring/service/Implementation/ClaimsServiceImpl.java
+++ src/main/java/tn/esprit/spring/service/Implementation/ClaimsServiceImpl.java
@@ -1,5 +1,50 @@
 package tn.esprit.spring.service.Implementation;
 
-public class ClaimsServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tn.esprit.spring.entity.Claims;
+import tn.esprit.spring.repository.ClaimsRepository;
+import tn.esprit.spring.service.Interface.ClaimsService;
+
+
+
+@Service
+public class ClaimsServiceImpl implements ClaimsService{
+	@Autowired
+    ClaimsRepository claimRep;
+
+	@Override
+	public List<Claims> retrieveAllClaims() {
+		@SuppressWarnings("unused")
+		List<Claims> claim=(List<Claims>)(claimRep.findAll());
+		return null;
+	}
+
+	@Override
+	public Claims addClaim(Claims c) {
+		claimRep.save(c);
+		return null;
+	}
+
+	@Override
+	public void deleteClaim(Long id) {
+		claimRep.deleteById(id);
+		
+	}
+
+	@Override
+	public Claims updateClaim(Claims c) {
+		
+		return null;
+	}
+
+	@Override
+	public Claims retrieveClaim(Long id) {
+		
+		return claimRep.findById(id).get();
+	}
 
 }
