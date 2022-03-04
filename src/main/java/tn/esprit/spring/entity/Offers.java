73614d6ee60d0bb45ff8366483de8c2b09f5bb8f
Yasmine kilani <yasmine.kilani@esprit.tn>
2022-03-04 02:13:28 +0100

    commit
    commit

--- src/main/java/tn/esprit/spring/entity/Offers.java
+++ src/main/java/tn/esprit/spring/entity/Offers.java
@@ -9,7 +9,11 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 import javax.persistence.ManyToOne;
+
+
+
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
