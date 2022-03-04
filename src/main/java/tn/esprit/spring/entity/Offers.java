144e88283f3b739b3ee4a1f12d6ad9cce48d5cd9
Ameni Latrach <Ameni.latrach@esprit.tn>
2022-03-04 01:27:06 +0100

    commit
    commit

--- src/main/java/tn/esprit/spring/entity/Offers.java
+++ src/main/java/tn/esprit/spring/entity/Offers.java
@@ -8,6 +8,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -18,7 +19,7 @@ import javax.persistence.TemporalType;
 public class Offers implements Serializable {
 	
 	
-
+@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	 @Column( name = "id_Offer")
 	private int IdOffer; 
