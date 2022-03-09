6cd69dac983c8a69cf29df506ff8fbf34f0372a4
Ameni Latrach <Ameni.latrach@esprit.tn>
2022-03-10 00:38:31 +0100

    commit

--- src/main/java/tn/esprit/spring/entity/Provision.java
+++ src/main/java/tn/esprit/spring/entity/Provision.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,6 +14,13 @@ import javax.persistence.OneToOne;
 public class Provision implements Serializable{
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
+
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+@Entity
+public class Provisions implements Serializable{
+@Id
+
 	private int id_provision;
 	private String type;
 	private String descreption;
