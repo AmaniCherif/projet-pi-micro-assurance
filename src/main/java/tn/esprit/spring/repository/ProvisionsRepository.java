ccdc8f54760123870b6009de6697565e0e35f60a
MalekJabeur <malek.jabeur@esprit.tn>
2022-03-10 02:50:54 +0100

    update ProvisionRepository

--- src/main/java/tn/esprit/spring/repository/ProvisionsRepository.java
+++ src/main/java/tn/esprit/spring/repository/ProvisionsRepository.java
@@ -3,8 +3,8 @@ package tn.esprit.spring.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import tn.esprit.spring.entity.Provisions;
+import tn.esprit.spring.entity.Provision;
 @Repository
-public interface ProvisionsRepository extends CrudRepository<Provisions, Integer>{
+public interface ProvisionsRepository extends CrudRepository<Provision, Integer>{
 
 }
