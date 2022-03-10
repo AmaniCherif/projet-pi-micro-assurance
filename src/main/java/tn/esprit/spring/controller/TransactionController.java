c3e69dd79404cede6ae6891456fd4c0a07fe1c06
Wael Guissouma <wael.guisouma@esprit.tn>
2022-03-10 22:19:13 +0100

    commit
    commit

--- src/main/java/tn/esprit/spring/controller/TransactionController.java
+++ src/main/java/tn/esprit/spring/controller/TransactionController.java
@@ -1,8 +1,9 @@
 package tn.esprit.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import tn.esprit.spring.entity.Transaction;
 import tn.esprit.spring.service.Interface.TransactionService;
@@ -10,8 +11,9 @@ import tn.esprit.spring.service.Interface.TransactionService;
 public class TransactionController {
 	@Autowired
 	TransactionService transactionservice;
-	@ResponseBody
-	public void addTransaction(@RequestBody Transaction t) {
-		transactionservice.addTransaction(t);
-	}
+	@PostMapping("/addTransaction")
+	public  Transaction addTransaction(@RequestBody Transaction t)
+		{transactionservice.addTransaction(t);
+		return t ;
+		}
 }
