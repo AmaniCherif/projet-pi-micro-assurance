ef68dccc0ace7d5f8a092465a8e6091a83de4439
MalekJabeur <malek.jabeur@esprit.tn>
2022-03-10 02:50:00 +0100

    update

--- src/main/java/tn/esprit/spring/entity/Provision.java
+++ src/main/java/tn/esprit/spring/entity/Provision.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 @Entity
-public class Provisions implements Serializable{
+public class Provision implements Serializable{
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private int id_provision;
