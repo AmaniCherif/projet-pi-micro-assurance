5fb7f74ee82a8a13a3c88f75eac6113a82f401fe
MalekJabeur <malek.jabeur@esprit.tn>
2022-03-10 21:09:44 +0100

    Update SinistreRepository.java

--- src/main/java/tn/esprit/spring/repository/SinistreRepository.java
+++ src/main/java/tn/esprit/spring/repository/SinistreRepository.java
@@ -1,9 +1,14 @@
 package tn.esprit.spring.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import tn.esprit.spring.entity.Sinistre;
+import tn.esprit.spring.entity.State_Sinistre_Claim;
 
 public interface SinistreRepository extends CrudRepository<Sinistre, Integer>{
+
+	List<Sinistre> findSinistreByState(State_Sinistre_Claim state_Sinistre_Claim);
 
 }
