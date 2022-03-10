166f7eecf53b64de2c059a1455856ecc053c01ad
MalekJabeur <malek.jabeur@esprit.tn>
2022-03-10 21:09:30 +0100

    Create ProvisionController.java

---
+++ src/main/java/tn/esprit/spring/controller/ProvisionController.java
@@ -0,0 +1,12 @@
+package tn.esprit.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tn.esprit.spring.service.Interface.IProvisionService;
+
+public class ProvisionController {
+
+	@Autowired
+	IProvisionService provisionService;
+	
+}
