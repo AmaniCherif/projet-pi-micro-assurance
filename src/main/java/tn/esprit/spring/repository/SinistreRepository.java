a9b34e3502a980fedc264f153f011a3a72a3612a
MalekJabeur <malek.jabeur@esprit.tn>
2022-03-11 05:35:28 +0100

    Update SinistreRepository.java

--- src/main/java/tn/esprit/spring/repository/SinistreRepository.java
+++ src/main/java/tn/esprit/spring/repository/SinistreRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import tn.esprit.spring.entity.ContractType;
 import tn.esprit.spring.entity.Sinistre;
 import tn.esprit.spring.entity.State_Sinistre_Claim;
 
@@ -15,5 +16,6 @@ import tn.esprit.spring.entity.State_Sinistre_Claim;
 public interface SinistreRepository extends CrudRepository<Sinistre, Integer>{
 	
 	List<Sinistre> findSinistreByState(State_Sinistre_Claim state_Sinistre_Claim);
-	
+	int findContractRequestDurationBySinistre(Integer id,ContractType str);
+	List<Sinistre> findSinistresByStateReclamation();
 }
