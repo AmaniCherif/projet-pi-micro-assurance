128dcca83c1f06975edb61408d50155c268a23cd
YosserBenameur <Yosser.Benameur@esprit.tn>
2022-03-25 12:35:36 +0100

    commit
    commit

--- src/main/java/tn/esprit/spring/entity/Claimtype.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.spring.entity;
-
-public enum Claimtype {
-	Response_Time,Application_loading,Screen_opening_beug,
-	Transaction_problems,Delay_contract_request,Lack_of_documents,
-	feedback,non_treated_sinister
-
-}
