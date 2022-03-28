a5d82008244184c14bfea300b07cef17150d7ab2
Ameni Latrach <Ameni.latrach@esprit.tn>
2022-03-28 04:23:14 +0200

    commit

--- src/main/java/tn/esprit/spring/controller/SinistreController.java
+++ src/main/java/tn/esprit/spring/controller/SinistreController.java
@@ -12,11 +12,11 @@ import tn.esprit.spring.service.Interface.ISinistreService;
 @RestController
 public class SinistreController {
 
-	@Autowired
-	ISinistreService sinistreservice;
+//	@Autowired
+//	ISinistreService sinistreservice;
 	@ResponseBody
 	public void addSinistre(@RequestBody Sinistre s) {
-		sinistreservice.addSinistre(s);
+//		sinistreservice.addSinistre(s);
 	}
 	
 }
--- src/main/java/tn/esprit/spring/repository/SinistreRepository.java
+++ src/main/java/tn/esprit/spring/repository/SinistreRepository.java
@@ -7,19 +7,14 @@ import org.springframework.stereotype.Repository;
 
 import tn.esprit.spring.entity.Sinistre;
 import tn.esprit.spring.entity.State_Sinistre_Claim;
-
+import tn.esprit.spring.entity.ContractType;
 
 
 
 @Repository
 public interface SinistreRepository extends CrudRepository<Sinistre, Integer>{
 
-	//List<Sinistre> findSinistreByState(State_Sinistre_Claim state_Sinistre_Claim);
-
-
-
-
-	
+	//List<Sinistre> findSinistreByState(State_Sinistre_Claim state_Sinistre_Claim);	
 	List<Sinistre> findSinistreByState(State_Sinistre_Claim state_Sinistre_Claim);
 	int findContractRequestDurationBySinistre(Integer id,ContractType str);
 	List<Sinistre> findSinistresByStateReclamation();
