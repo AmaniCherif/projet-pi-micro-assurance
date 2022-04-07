1865b73d7542a394f552d1853c6a73f7285d65bb
YosserBenameur <Yosser.Benameur@esprit.tn>
2022-04-07 15:52:44 +0200

    commit

--- src/main/java/tn/esprit/spring/repository/IRendement.java
+++ src/main/java/tn/esprit/spring/repository/IRendement.java
@@ -1,13 +1,16 @@
 package tn.esprit.spring.repository; //Youyou_rendement_repository
 
-import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import org.springframework.data.repository.CrudRepository;
+
 import org.springframework.stereotype.Repository;
 
 import tn.esprit.spring.entity.Rendement;
 
 @Repository
-public interface IRendement extends JpaRepository <Rendement , Integer>{
-	
-	Rendement findByAnnee(int annee); //kol_3am_3andi_rendement_spécifique
-
+public interface IRendement extends CrudRepository<Rendement, Integer>  {
+	/*@Query("select * from rendement r where r.annee =:a ")
+	Rendement fin(@Param("a") int a); //kol_3am_3andi_rendement_spécifique*/
+	Rendement findByAnnee(  int annee);
 }
