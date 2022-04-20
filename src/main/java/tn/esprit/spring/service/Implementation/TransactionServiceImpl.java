9ace4adae41b3fd3fea60ce625b25191e1ac9959
Wael Guissouma <wael.guisouma@esprit.tn>
2022-04-20 23:19:04 +0100

    commit
    commit

--- src/main/java/tn/esprit/spring/service/Implementation/TransactionServiceImpl.java
+++ src/main/java/tn/esprit/spring/service/Implementation/TransactionServiceImpl.java
@@ -33,8 +33,16 @@ public class TransactionServiceImpl implements TransactionService{
 	if (!ObjectUtils.isEmpty(contract))
 		t.setContract(contract);
 //	    contractRepository.save(contract);
-			
-			
+	           if (contract.getUser().getClassification() == null) {}
+	           else if (contract.getUser().getClassification().name().equals("Gold"))
+				t.setTransactionAmount(t.getTransactionAmount()-((t.getTransactionAmount()*20)/100));
+			else if (contract.getUser().getClassification().name().equals("Silver")) 
+				t.setTransactionAmount(t.getTransactionAmount()-((t.getTransactionAmount()*15)/100));
+			else if (contract.getUser().getClassification().name().equals("Bronz"))
+				t.setTransactionAmount(t.getTransactionAmount()-((t.getTransactionAmount()*10)/100));
+		
+
+
 		transactionRepository.save(t);
 		
 		return t;
